fd85e04feb890ceef35340bcec81974a48f934b0
Varun <aggarwalvarun58@gmail.com>
2022-01-10 20:42:19 +0530

    Created basic templates for all components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
+  recipes: Recipe[] = [
+    new Recipe("A Test Recipe", "This is simply a test", "https://get.pxhere.com/photo/tomato-food-cuisine-pasta-dishware-ingredient-tableware-dish-plate-recipe-meal-serveware-vegetable-produce-garnish-brunch-breakfast-vegetarian-food-culinary-art-side-dish-mixture-kitchen-utensil-italian-food-staple-food-bowl-pasta-salad-cooking-leaf-vegetable-orecchiette-supper-Food-group-platter-fast-food-snack-whole-food-mixing-bowl-superfood-lunch-salad-al-dente-la-carte-food-1636257.jpg"),
+    new Recipe("A Test Recipe", "This is simply a test", "https://get.pxhere.com/photo/tomato-food-cuisine-pasta-dishware-ingredient-tableware-dish-plate-recipe-meal-serveware-vegetable-produce-garnish-brunch-breakfast-vegetarian-food-culinary-art-side-dish-mixture-kitchen-utensil-italian-food-staple-food-bowl-pasta-salad-cooking-leaf-vegetable-orecchiette-supper-Food-group-platter-fast-food-snack-whole-food-mixing-bowl-superfood-lunch-salad-al-dente-la-carte-food-1636257.jpg"),
+    new Recipe("A Test Recipe", "This is simply a test", "https://get.pxhere.com/photo/tomato-food-cuisine-pasta-dishware-ingredient-tableware-dish-plate-recipe-meal-serveware-vegetable-produce-garnish-brunch-breakfast-vegetarian-food-culinary-art-side-dish-mixture-kitchen-utensil-italian-food-staple-food-bowl-pasta-salad-cooking-leaf-vegetable-orecchiette-supper-Food-group-platter-fast-food-snack-whole-food-mixing-bowl-superfood-lunch-salad-al-dente-la-carte-food-1636257.jpg")
+    
+  ]
   constructor() { }
 
   ngOnInit(): void {
